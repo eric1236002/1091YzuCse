e7d97d8f3d329b4758c5bb5c0d3b2c0ca0348322
minyuan1119 <stji9gjz2@gmail.com>
2020-11-22 18:28:06 +0800

    .

--- DataStructures - Tinin/HW2/Source.cpp
+++ DataStructures - Tinin/HW2/Source.cpp
@@ -34,21 +34,21 @@ struct node
 
 string progStr(string str)
 {
+    // 移除前面及後面的空白
     while (!str.empty() && str.back() == ' ') str.pop_back();
-    stringstream ss(str);
-    string tmp, tmp1;
-    ss >> tmp;
-    while (!ss.eof()) ss >> tmp1, tmp += " " + tmp1;
-    if (tmp.empty()) return tmp;
-    while (!tmp.empty() && (tmp.front() == '"' || tmp.front() == ' ')) tmp.erase(tmp.begin());
-    while (!tmp.empty() && (tmp.back() == '"' || tmp.back() == ' ')) tmp.pop_back();
-    return tmp;
+    while (!str.empty() && str.front() == ' ') str.erase(str.begin());
+
+    // 移除前後的雙引號
+    if (!str.empty() && str.back() == '"') str.pop_back();
+    if (!str.empty() && str.front() == '"') str.erase(str.begin());
+
+    return str;
 }
 
-class paser
+class parser
 {
 public:
-    paser() { root = make_shared<node>(); }
+    parser() { root = make_shared<node>(); }
 
     void read(vector<string>& v1, vector<string>& v2)
     {
@@ -59,9 +59,9 @@ public:
 
     void put()
     {
-        bool c = true;
+        bool stop = false;
+        DQuotes = false;
         stack<shared_ptr<node>> nodes;
-        
         stack<bool> islist;
 
         nodes.push(root);
@@ -71,53 +71,60 @@ public:
         auto valtmp = make_shared<node>();
         shared_ptr<node> tmp;
 
-        for (size_t i = 0; i < data.size() && c; ++i) for (size_t j = 0; j < data[i].length() && c; ++j)
+        for (auto& v : data) for (auto& ch : v)
         {
-            switch (data[i][j])
+            // 避免雙引號中出現關鍵字造成錯誤，如 "1[5" 或 "\"15"
+            if (DQuotes && !(ch == '"' && str.back() != '\\'))
             {
+                str.push_back(ch);
+                continue;
+            }
+            switch (ch)
+            {
+            case '"':
+                DQuotes ^= 1;
+                str.push_back(ch);
+                break;
+
             case '{':
                 nodes.push(valtmp);
                 islist.push(false);
                 valtmp = make_shared<node>();
                 break;
+
             case '}':
-                if (!valtmp->key.empty())
+                if (!str.empty())
                 {
                     valtmp->val = progStr(str);
-                    if (!islist.top())
-                    {
-                        nodes.top()->v.push_back(valtmp);
-                        valtmp = make_shared<node>();
-                    }
+                    if (islist.top()) nodes.top()->l.push_back(valtmp);
+                    else nodes.top()->v.push_back(valtmp);
+                    valtmp = make_shared<node>();
                 }
 
                 tmp = nodes.top();
                 nodes.pop();
                 islist.pop();
 
-                if (!islist.top()) nodes.top()->v.push_back(tmp);
-                else nodes.top()->l.push_back(tmp);
-                
+                if (islist.top()) nodes.top()->l.push_back(tmp);
+                else nodes.top()->v.push_back(tmp);
+
                 str.clear();
-                if (nodes.size() == 1) c = false;
+                if (nodes.size() == 1) stop = true;
                 break;
+
             case '[':
                 valtmp->islist = true;
                 nodes.push(valtmp);
                 islist.push(true);
                 valtmp = make_shared<node>();
                 break;
+
             case ']':
-                if (!valtmp->key.empty())
+                if (!str.empty())
                 {
                     valtmp->val = progStr(str);
-                    nodes.top()->v.push_back(valtmp);
-                    valtmp = make_shared<node>();
-                }
-                else
-                {
-                    valtmp->val = progStr(str);
-                    nodes.top()->l.push_back(valtmp);
+                    if (nodes.top()->islist) nodes.top()->l.push_back(valtmp);
+                    else nodes.top()->v.push_back(valtmp);
                     valtmp = make_shared<node>();
                 }
 
@@ -125,105 +132,100 @@ public:
                 nodes.pop();
                 islist.pop();
 
-                nodes.top()->v.push_back(tmp);
+                if (nodes.top()->islist) nodes.top()->l.push_back(tmp);
+                else nodes.top()->v.push_back(tmp);
 
                 str.clear();
                 break;
+
             case ':':
                 valtmp->key = progStr(str);
                 str.clear();
                 break;
+
             case ',':
                 if (!str.empty())
                 {
-                    if (islist.top())
-                    {
-                        valtmp->val = progStr(str);
-                        nodes.top()->l.push_back(valtmp);
-                        valtmp = make_shared<node>();
-                    }
-                    else
-                    {
-                        if (!valtmp->key.empty())
-                        {
-                            valtmp->val = progStr(str);
-                            nodes.top()->v.push_back(valtmp);
-                            valtmp = make_shared<node>();
-                        }
-                    }
+                    valtmp->val = progStr(str);
+                    if (nodes.top()->islist) nodes.top()->l.push_back(valtmp);
+                    else nodes.top()->v.push_back(valtmp);
+                    valtmp = make_shared<node>();
                 }
 
                 str.clear();
                 break;
+
             default:
-                str.push_back(data[i][j]);
+                str.push_back(ch);
                 break;
             }
+
+            if (stop) return;
         }
     }
 
     void print()
     {
-        bool first = false, out = false;
+        bool first = false;
         string str;
         for (auto& s : search)
         {
+            out = false;
             vector<string> v;
             stringstream ss(s);
             while (getline(ss, str, '>')) v.emplace_back(str);
             if (first) cout << endl;
             _search(v);
             first = true;
+            if (!out) cout << endl;
         }
     }
 
     void _search(vector<string>& v)
     {
         if (v.empty()) return;
-        if (v.size() == 1) dfs1(root, v.front());
+        if (v.size() == 1) dfs1(root, v[0]);
         else dfs2(root, v, 0);
     }
 
-    void dfs1(shared_ptr<node> r, string& str)
+    // 搜尋只有一層
+    void dfs1(shared_ptr<node>& r, string& str)
     {
-        if (r->key == str && r->islist) for (auto& p1 : r->l) print2(p1);
-        else
+        if (r->key == str)
         {
-            if (r->key == str) cout << r->val << endl;
-            for (auto& p1 : r->v) dfs1(p1, str);
-            for (auto& p1 : r->l) dfs1(p1, str);
+            if (r->islist) listPrint(r);
+            else if(!r->val.empty()) cout << r->val << endl, out = true;
         }
+        for (auto& p1 : r->v) dfs1(p1, str);
+        for (auto& p1 : r->l) dfs1(p1, str);
     }
 
-    void print2(shared_ptr<node>r)
-    {
-        if (r->key.empty()) cout << r->val << endl;
-        else
-        {
-            for (auto& p1 : r->l) print2(p1);
-            for (auto& p1 : r->v) print2(p1);
-        }
-    }
-
-    void dfs2(shared_ptr<node>r, vector<string>& v, int cnt)
+    // 搜尋大於一層
+    void dfs2(shared_ptr<node>& r, vector<string>& v, int cnt)
     {
         if (r->key == v[cnt])
         {
             if (cnt == v.size() - 1)
             {
-                if (r->islist) for (auto& p1 : r->l) print2(p1);
-                else cout << r->val << endl;
+                if (r->islist) listPrint(r);
+                else if (!r->val.empty()) cout << r->val << endl, out = true;
+                return;
             }
-            else
-            {
-                for (auto& p1 : r->v) dfs2(p1, v, cnt + 1);
-                for (auto& p1 : r->l) dfs2(p1, v, cnt + 1);
-            }
+            else ++cnt;
         }
-        else
+        for (auto& p1 : r->v) dfs2(p1, v, cnt);
+        for (auto& p1 : r->l) dfs2(p1, v, cnt);
+    }
+
+    void listPrint(shared_ptr<node>& r)
+    {
+        for (auto& n : r->l)
         {
-            for (auto& p1 : r->v) dfs2(p1, v, cnt);
-            for (auto& p1 : r->l) dfs2(p1, v, cnt);
+            if (n->l.empty() && n->v.empty()) // n 為 leaf node
+            {
+                cout << n->val << endl;
+                out = true;
+            }
         }
     }
 
@@ -231,6 +233,8 @@ private:
     vector<string> data;
     vector<string> search;
     shared_ptr<node> root;
+    bool out;
+    bool DQuotes;
 };
 
 int main(int argc, char* argv[])
@@ -243,8 +247,8 @@ int main(int argc, char* argv[])
     //ifstream infile2(argv[2]);
     while (getline(infile1, str)) v1.emplace_back(str);
     while (getline(infile2, str)) v2.emplace_back(str);
-
-    paser p;
+    
+    parser p;
     p.read(v1, v2);
     p.print();
 }
